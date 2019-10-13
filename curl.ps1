7d47fc2d16c73cad4ea07f7cf502ef4f733aea88
mushimaruko <mushimaruko@yahoo.co.jp>
2019-10-13 19:19:26 +0900

    20191013-2

--- curl.ps1
+++ curl.ps1
@@ -2,8 +2,26 @@
 
 $DATABASEDOTCOM_CLIENT_ID="3MVG9pe2TCoA1Pf6b.MWGLscN_7l51xXAF79OFZmjqfNPto3JohTujho1eWZErHffpBnRYVH8889zpruvUOp0"
 $DATABASEDOTCOM_CLIENT_SECRET="D57E4EBC49CB0CACF946BAECD60514F3284FB917215C88667F79A679949B4D77"
-$DATABASEDOTCOM_CLIENT_USERNAME="mushimaruko@gmail.com"
+$DATABASEDOTCOM_CLIENT_USERNAME="mushimaruko%40gmail.com"
 $DATABASEDOTCOM_CLIENT_AUTHENTICATE_PASSWORD="#42Mushi3"
 $DATABASEDOTCOM_HOST="login.salesforce.com"
-
-curl -s https://$DATABASEDOTCOM_HOST/services/oauth2/token -d "grant_type=password" -d "client_id=$DATABASEDOTCOM_CLIENT_ID" -d "client_secret=$DATABASEDOTCOM_CLIENT_SECRET" -d "username=$DATABASEDOTCOM_CLIENT_USERNAME" -d "password=$DATABASEDOTCOM_CLIENT_AUTHENTICATE_PASSWORD"
+$h = @{grant_type="password";
+    clident_id=$DATABASEDOTCOM_CLIENT_ID;
+    client_secret=$DATABASEDOTCOM_CLIENT_SECRET;
+    username=$DATABASEDOTCOM_CLIENT_USERNAME;
+    password=$DATABASEDOTCOM_CLIENT_AUTHENTICATE_PASSWORD}
+try{
+Invoke-WebRequest -Uri  https://$DATABASEDOTCOM_HOST/services/oauth2/token -Method Post  -Body $h
+}
+catch {
+    echo '### Inside catch ###'
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    $result = $_.Exception.Response.GetResponseStream()
+    echo '## result2 ##' $result
+   $reader = New-Object System.IO.StreamReader($result)
+    echo '## reader ##' $reader 
+   $responseBody = $reader.ReadToEnd();
+    echo '## responseBody ##' $responseBody
+   
+}
