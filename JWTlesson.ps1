ac969062157184351628579328088cb90d507609
mushimaruko <mushimaruko@yahoo.co.jp>
2019-10-14 17:10:40 +0900

    20191014-2

--- JWTlesson.ps1
+++ JWTlesson.ps1
@@ -1,18 +1,29 @@
-$headerString='{
-  "alg" : "HS256",
-  "typ" : "JWT"
+$HeaderString='{
+  "alg" : "RS256"
+  
 }'
-$headerByte = ([System.Text.Encoding]::Default).GetBytes($headerString)
-$headerBase64 = $([convert]::ToBase64String($headerByte))
-
+$HeaderByte = ([System.Text.Encoding]::Default).GetBytes($HeaderString)
+$HeaderBase64 = $([convert]::ToBase64String($HeaderByte))
+$exptime = [int][double]::parse((Get-Date -Date $((Get-Date).addseconds($ValidforSeconds).ToUniversalTime()) -UFormat %s)) + 280
 $LoginMap = @{
   iss="3MVG9pe2TCoA1Pf6b.MWGLscN_7l51xXAF79OFZmjqfNPto3JohTujho1eWZErHffpBnRYVH8889zpruvUOp0";
   aud='https://login.salesforce.com';
   sub='mushimaruko@gmail.com';
-  exp='1571029200';
+  exp=$exptime;
 };
 $LoginJson = $($LoginMap | ConvertTo-Json)
 $LoginByte = $(([System.Text.Encoding]::Default).GetBytes($LoginJson) )
 $LoginBase64 = $([convert]::ToBase64String($LoginByte))
 
-$key = $(cat .\server.key)
+$JWTRequest = $HeaderBase64 + "." + $LoginBase64
+$arg = New-Object System.Security.Cryptography.HMACSHA256
+$arg.Key = [System.Text.Encoding]::UTF8.GetBytes('.\server.key')
+$Signature = [Convert]::ToBase64String($arg.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($JWTRequest))).Split('=')[0].Replace('+', '-').Replace('/', '_')
+$jwt = $JWTRequest + '.' + $Signature
+$b = @{grant_type='urn:ietf:params:oauth:grant-type:jwt-bearer';
+    assertion=$jwt
+}
+$EndPoint = 'https://login.salesforce.com/services/oauth2/token'
+Invoke-WebRequest -Uri $EndPoint -Method Post -Body $b
+
+#$key = $(cat .\server.key)
